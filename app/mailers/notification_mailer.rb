8f79e779be524b8d5a3a2728b2dc3dd9cc5f1982
James Sherriff <jamessherriff@warwickshire.gov.uk>
2016-09-29 14:08:09 +0100

    Bug fixes

--- app/mailers/notification_mailer.rb
+++ app/mailers/notification_mailer.rb
@@ -2,7 +2,7 @@ class NotificationMailer < ApplicationMailer
 	require 'mailgun'
 	def calendar_change_email(params)
 		@params = params
-    mg_client = Mailgun::Client.new ENV['action_gun_key']
+    mg_client = Mailgun::Client.new "key-58f9a358ddb5394fcb0dcccf0fe450a3"
     message_params = {:from => "postmaster@sandbox2bce1d2550dd4a858452a68a5269f473.mailgun.org", :to => "jamessherriff@warwickshire.gov.uk", :subject => "Calendar change", :text => "Your calendar has changed"}
 		mg_client.send_message "lan-lounge.herokuapp.com", message_params
 	end
