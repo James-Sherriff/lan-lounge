25ce81219901a437ebe85b1833d3f3c93da11183
James Sherriff <jamessherriff@warwickshire.gov.uk>
2016-09-29 12:53:02 +0100

    Changed action mailer default url options

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -17,7 +17,7 @@ Rails.application.configure do
   config.action_mailer.raise_delivery_errors = false
   
   # Set the url options for the mailer
-  config.action_mailer.default_url_options = {host: 'localhost', post: 3000}
+  config.action_mailer.default_url_options = {host: 'lan-lounge.herokuapp.com', post: 25}
 
   # Print deprecation notices to the Rails logger.
   config.active_support.deprecation = :log
