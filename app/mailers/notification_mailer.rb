a71c9f2a640ae669b4b3486e2eda759d6250a87f
James Sherriff <jamessherriff@warwickshire.gov.uk>
2016-09-29 14:11:46 +0100

    Config change

--- app/mailers/notification_mailer.rb
+++ app/mailers/notification_mailer.rb
@@ -4,6 +4,6 @@ class NotificationMailer < ApplicationMailer
 		@params = params
     mg_client = Mailgun::Client.new "key-58f9a358ddb5394fcb0dcccf0fe450a3"
     message_params = {:from => "postmaster@sandbox2bce1d2550dd4a858452a68a5269f473.mailgun.org", :to => "jamessherriff@warwickshire.gov.uk", :subject => "Calendar change", :text => "Your calendar has changed"}
-		mg_client.send_message "lan-lounge.herokuapp.com", message_params
+		mg_client.send_message "smtp.mailgun.org", message_params
 	end
 end
