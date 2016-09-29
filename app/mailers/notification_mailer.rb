26f554929b6dfd4c7ec47b1e92350fb13e33f3ba
James Sherriff <jamessherriff@warwickshire.gov.uk>
2016-09-29 14:20:52 +0100

    Config change

--- app/mailers/notification_mailer.rb
+++ app/mailers/notification_mailer.rb
@@ -1,5 +1,5 @@
 class NotificationMailer < ApplicationMailer
-	require 'mailgun'
+	require 'mailgun_rails'
 	def calendar_change_email(params)
 		@params = params
     mail({ :from => "postmaster@sandbox2bce1d2550dd4a858452a68a5269f473.mailgun.org", :to => "jamessherriff@warwickshire.gov.uk", :subject => "Calendar update", :text => "Calendar updates"})
